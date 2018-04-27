66ce4d2bb2fe6111000f165a25eb92feec902813
DmitryDzz <DzakhovDS@gmail.com>
2018-04-27 13:09:34 +0300

    SnapDropZone eventes

---
+++ Assets/SnapDropTestEvents.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class SnapDropTestEvents : MonoBehaviour
+{
+    public void OnEnterZone(object obj, SnapDropZoneEventArgs args)
+    {
+        Debug.Log("OnEnterZone " + obj.GetType().Name + " SnapDuration=" + ((VRTK_SnapDropZone) obj).snapDuration);
+    }
+
+    public void OnExitZone(object obj, SnapDropZoneEventArgs args)
+    {
+        Debug.Log("OnExitZone");
+    }
+
+    public void OnSnappedZone(object obj, SnapDropZoneEventArgs args)
+    {
+        Debug.Log("OnSnappedZone");
+    }
+
+    public void OnUnsnappedZone(object obj, SnapDropZoneEventArgs args)
+    {
+        Debug.Log("OnUnsnappedZone");
+    }
+}
